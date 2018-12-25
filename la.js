b27f3835934127a29cd3cb90d5ec6986219bc4e8
Krasimir Tsonev <krasimir@outset.ws>
2018-12-25 07:36:53 +0200

    It was all wrong in the beginning. The direction value should be flipped.

--- la.js
+++ la.js
@@ -114,14 +114,14 @@
         e.innerHTML = '<div class="la-name">' + item.name + '</div>';
         addLinks(e, [ ['remove', '<svg width="14" height="14" viewBox="0 0 1792 1792"><path d="M1490 1322q0 40-28 68l-136 136q-28 28-68 28t-68-28l-294-294-294 294q-28 28-68 28t-68-28l-136-136q-28-28-28-68t28-68l294-294-294-294q-28-28-28-68t28-68l136-136q28-28 68-28t68 28l294 294 294-294q28-28 68-28t68 28l136 136q28 28 28 68t-28 68l-294 294 294 294q28 28 28 68z"/></svg>', 'la-remove'], ], item, parent);
       } else {
-        e.innerHTML = '<div class="la-children" style="grid-template-' + (item.direction === 'horizontal' ? 'rows' : 'columns') + ': repeat(' + item.elements.length + ', 1fr);"></div>';
+        e.innerHTML = '<div class="la-children" style="grid-template-' + (item.direction === 'vertical' ? 'rows' : 'columns') + ': repeat(' + item.elements.length + ', 1fr);"></div>';
         item.elements.forEach(i => e.querySelector('.la-children').appendChild(renderItem(i, item)))
       }
       addLinks(e, [
-        ['vertical:left', '', 'la-left'],
-        ['horizontal:top', '', 'la-top'],
-        ['horizontal:bottom', '', 'la-bottom'],
-        ['vertical:right', '', 'la-right']
+        ['horizontal:left', '', 'la-left'],
+        ['vertical:top', '', 'la-top'],
+        ['vertical:bottom', '', 'la-bottom'],
+        ['horizontal:right', '', 'la-right']
       ], item, parent);
       return e;
     }
